29a409a3214285d9aedb72fc9661166e5477b33c
thekaranjit <thekaranjit@outlook.com>
2022-06-10 10:13:12 -0400

    Working

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function getResult(){
+function getresult() {
 
       let yinc = document.getElementById('yearly-income').value;
 
       let nincome = document.getElementById('need').value;
 
-      let in = document.getElementById('invest').value;
+      let inv = document.getElementById('invest').value;
 
       let ifix = document.getElementById('fix').value;
 
-      // document.getElementById("result").innerHTML=yinc;
-      console.log(yinc);
+      document.getElementById("result").innerHTML=yinc;
+      // console.log(yinc);
 }
