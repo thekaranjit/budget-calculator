c110d459c53a643977f1d42008cd0b735ff4aa64
thekaranjit <thekaranjit@outlook.com>
2022-06-09 21:38:16 -0400

    Basic setup

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function getresult(){
 
+      let yinc = document.getElementById('yearly-income').value;
+
+      let nincome = document.getElementById('need').value;
+
+      let in = document.getElementById('invest').value;
+
+      let ifix = document.getElementById('fix').value;
+
+      document.getElementById("result").innerHTML=yinc;
+
+}
